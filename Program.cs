da4eb8231a6ec28139729e5f8cd04b441cee0ba3
Guilherme Felipe Molon <gfmolon@gmail.com>
2024-05-24 17:37:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Stopwatch
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            App();
+        }
+
+        static void App()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o Tempo");
+
+            int tempoInicial = 0;
+            int tempoLimite = int.Parse(Console.ReadLine());
+
+            while (tempoLimite != tempoInicial)
+            {
+                Console.Clear();
+                Console.WriteLine(tempoInicial);
+                ++tempoInicial;
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Cronometro Finalizado.");
+
+        }
+
+    }
+}
